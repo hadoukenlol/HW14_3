9a0d71cf3e9219aafb1ccdf44e933bb9c7abe8ac
Мейлис Клычев <noobs.ololo@gmail.com>
2023-07-30 16:29:28 +0300

    Добавьте файлы проекта.

---
+++ HW14_3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace HW14_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            // делаем сортировку
+            var newList = phoneBook.OrderBy(s => s.Name).ThenBy(s => s.LastName);
+            foreach (var item in newList)
+                Console.WriteLine($"{item.Name} {item.LastName}");
+        }
+    }
+    public class Contact // модель класса
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
